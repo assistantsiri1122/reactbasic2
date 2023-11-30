a67e4b294053aa914b0eb9bb7387053d9c3045bc
Don <arcsagar15@gmail.com>
2023-11-30 14:17:06 +0530

    check002

--- basic2.js
+++ basic2.js
@@ -1,11 +1,13 @@
-const obj1={
-    name:'sagar',
-    age:25,
-    class:"react",
-    obj2:{
-        'key':'value'
-    },
-    name:"sagar2",
-    name:"sagar3"
-};
-console.log('obj1',obj1);
+// const obj1={
+//     name:'sagar',
+//     age:25,
+//     class:"react",
+//     obj2:{
+//         'key':'value'
+//     },
+//     name:"sagar2",
+//     name:"sagar3"
+// };
+// console.log('obj1',obj1);
+
+console.log(1+5);
